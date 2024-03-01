4b0aa3b6231b4b40d60167071e65e184df8eacdd
UgoInvernini01 <ugoinvernini99@gmail.com>
2024-03-01 19:34:20 +0100

    modifica

--- script/app.js
+++ script/app.js
@@ -8,13 +8,21 @@ item.addEventListener("click",()=>{
 
     })
 
-/*
+    window.addEventListener("scroll",()=>{
+
+        if(window.scrollY>300){
+            document.body.classList.add("scrolled-down")
+        }else{
+        
+            document.body.classList.remove("scrolled-down")
+        }
+        
+        })
+
     gsap.to(".logo", {opacity:1,duration:1 }); //duration
     gsap.from(".menu li",{opacity:0,y:60,duration:1.5,ease: Power3.easeOut,stragger:0.2})
-    gsap.to(".hero__text", {x:200,duration:2,delay:0.3, ease: Power3.easeOut }); //duration
+    
+    gsap.from(".hero__text", {x:-100,duration:2,delay:0.3, ease: Power3.easeOut }); //duration
+    gsap.from(".hero__image", {scale:0.7,duration:4,ease: Power3.easeOut,opacity:0}); //from
 
-    gsap.from(".hero__image", {scale:0.4,duration:4,ease: Power3.easeOut,opacity:0}); //from
-
-    gsap.from(".col-40 img",{opacity:0,x:-500,duration:3})
- 
-*/
+    
